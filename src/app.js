3a018d3eec995b7c302dd1ba1840b752366635fa
AEstrada <allan.estrada9@gmail.com>
2023-10-25 22:20:18 -0600

    Agregando Politicas CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routers/auth.routes.js";
 import taskRoutes from "./routers/tasks.routes.js";
@@ -8,6 +9,7 @@ import taskRoutes from "./routers/tasks.routes.js";
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
