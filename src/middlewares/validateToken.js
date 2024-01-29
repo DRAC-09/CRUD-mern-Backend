4cf48be02415bac5d73ce746193aa0d6acf638bc
drac-09 <allan.estrada9@gmail.com>
2024-01-28 21:32:46 -0600

    Verificando token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,8 +2,12 @@ import jwt from "jsonwebtoken";
 
 export const authRequired = (req, res, next) => {
   const { token } = req.cookies;
-  if (!token)
-    return res.status(401).json({ message: "No token, authorization denied" });
+  const requestData = {
+    headers: req.headers,
+    cookies: req.cookies,
+  };
+  if (!token) return res.status(401).json({ requestData });
+  // return res.status(401).json({ message: "No token, authorization denied" });
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: "Invalid Token" });
