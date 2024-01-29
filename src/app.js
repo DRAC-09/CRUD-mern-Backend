fc42939a5c88012e268c1065b5bb307c6b20ce6c
drac-09 <allan.estrada9@gmail.com>
2024-01-28 22:17:03 -0600

    Corrigiendo direccion del frontend

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: `http://localhost:5173`,
+    origin: "https://task-frontend-mern.vercel.app/",
     credentials: true,
   })
 );
