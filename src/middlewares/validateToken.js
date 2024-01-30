5888a69da32d5eb016d8ddc9487560e9a13f2936
drac-09 <allan.estrada9@gmail.com>
2024-01-29 19:29:03 -0600

    Actualizando token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,12 +2,11 @@ import jwt from "jsonwebtoken";
 
 export const authRequired = (req, res, next) => {
   const { token } = req.headers;
-  const requestData = {
-    headers: req.headers,
-    cookies: req.cookies,
-  };
-  return res.status(401).json({ requestData });
-
+  // const requestData = {
+  //   headers: req.headers,
+  //   cookies: req.cookies,
+  // };
+  if (!token) return res.status(401).json({ message: "Invalid Token" });
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: "Invalid Token" });
     req.user = user;
